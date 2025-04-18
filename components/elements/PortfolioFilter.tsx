f3badcb5313c0948b09c99db0d2a7576465b5c06
bettyp23 <bp23e@fsu.edu>
2025-04-17 20:55:29 -0400

    remove images

--- components/elements/PortfolioFilter.tsx
+++ components/elements/PortfolioFilter.tsx
@@ -39,21 +39,12 @@ export default function PortfolioFilter() {
 		<>
 
 			<div className="container">
-				{/* <div className="text-start">
-					{/* <div className="button-group filter-button-group filter-menu-active">
-						<button className={activeBtn("*")} onClick={handleFilterKeyChange("*")}>All Projects</button>
-						<button className={activeBtn("brand")} onClick={handleFilterKeyChange("brand")}>Branding</button>
-						<button className={activeBtn("webdesign")} onClick={handleFilterKeyChange("webdesign")}>Web Design</button>
-						<button className={activeBtn("ui")} onClick={handleFilterKeyChange("ui")}>UI/UX</button>
-						<button className={activeBtn("app")} onClick={handleFilterKeyChange("app")}>App Dev</button>
-					</div> }
-				</div> */}
 				<div className="row masonry-active justify-content-between mt-6">
 					<div className="grid-sizer" />
 					<div className="filter-item col-lg-6 col-12 brand ui app">
 						<div className="project-item rounded-4 overflow-hidden position-relative p-md-4 p-3 bg-white">
 							<div>
-								<img className="rounded-3 w-100 zoom-img" src="/assets/imgs/projects/projects-1/img-1.png" alt="infinia" />
+								{/* <img className="rounded-3 w-100 zoom-img" src="/assets/imgs/projects/projects-1/img-1.png" alt="infinia" /> */}
 							</div>
 							<div className="d-flex align-items-center mt-4">
 								<Link href="https://github.com/bettyp23/AI-NLP-Analysis-and-Stock-Data-Script" className="project-card-content">
@@ -70,7 +61,7 @@ export default function PortfolioFilter() {
 					<div className="filter-item col-lg-6 col-12 webdesign brand dataanalysis brand">
 						<div className="project-item rounded-4 overflow-hidden position-relative p-md-4 p-3 bg-white">
 							<div>
-								<img className="rounded-3 w-100 zoom-img" src="/assets/imgs/projects/projects-1/img-2.png" alt="infinia" />
+								{/* <img className="rounded-3 w-100 zoom-img" src="/assets/imgs/projects/projects-1/img-2.png" alt="infinia" /> */}
 							</div>
 							<div className="d-flex align-items-center mt-4">
 								<Link href="https://github.com/bettyp23/AI-Powered-Stock-Sentiment-Investment-Advisor" className="project-card-content">
@@ -86,7 +77,7 @@ export default function PortfolioFilter() {
 					<div className="filter-item col-lg-6 col-12 ui app">
 						<div className="project-item rounded-4 overflow-hidden position-relative p-md-4 p-3 bg-white">
 							<div>
-								<img className="rounded-3 w-100 zoom-img" src="/assets/imgs/projects/projects-1/img-3.png" alt="infinia" />
+								{/* <img className="rounded-3 w-100 zoom-img" src="/assets/imgs/projects/projects-1/img-3.png" alt="infinia" /> */}
 							</div>
 							<div className="d-flex align-items-center mt-4">
 								<div className="project-card-content">
@@ -102,7 +93,7 @@ export default function PortfolioFilter() {
 					<div className="filter-item col-lg-6 col-12 app app dataanalysis brand">
 						<div className="project-item rounded-4 overflow-hidden position-relative p-md-4 p-3 bg-white">
 							<div>
-								<img className="rounded-3 w-100 zoom-img" src="/assets/imgs/projects/projects-1/img-4.png" alt="infinia" />
+								{/* <img className="rounded-3 w-100 zoom-img" src="/assets/imgs/projects/projects-1/img-4.png" alt="infinia" /> */}
 							</div>
 							<div className="d-flex align-items-center mt-4">
 								<Link href="https://github.com/bettyp23/Project.0_SpaceInvaders" className="project-card-content">
