277fa38c37dc9f3fc4f241b86ee08d8293b3b56b
bettyp23 <bp23e@fsu.edu>
2025-04-18 09:23:11 -0400

    alignement

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -262,9 +262,9 @@ export default function Services() {
 						</div>
 					</div>
 					{/* Contact 1*/}
-					<section id="contact" className="section-contact-1 bg-900 position-relative pt-150 pb-lg-250 pb-150 overflow-hidden">
+					<section id="contact" className="section-contact-1 bg-900 position-relative pt-150 pb-lg-250 pb-250">
 						<div className="text-center">
-  <h3 className="ds-3 mt-3 mb-3 text-primary-1">Get in touch</h3>
+							<h3 className="ds-3 mt-3 mb-3 text-primary-1">Get in touch</h3>
 							<span className="fs-5 fw-medium text-200 mb-4 d-block text-center">
 							I'm always open to new projects and collaborating with innovative minds!
 							</span>
@@ -284,9 +284,10 @@ export default function Services() {
 							</a>
 							</div>
 						</div>
-						<div className="scroll-move-right position-absolute bottom-0 start-50 translate-middle-x bg-900 overflow-hidden">
+
+						<div className="scroll-move-right position-absolute bottom-0 start-50 translate-middle-x bg-900" style={{ zIndex: 0 }}>
 							<div className="wow img-custom-anim-top">
-								<h3 className="stroke fs-280 text-lowercase text-900 mb-0 lh-1">Betty Phipps</h3>
+							<h3 className="stroke fs-280 text-lowercase text-900 mb-0 lh-1">Betty Phipps</h3>
 							</div>
 						</div>
 					</section>
--- components/sections/Contact1.tsx
+++ components/sections/Contact1.tsx
@@ -3,10 +3,10 @@ import Link from "next/link"
 
 export default function Contact1() {
 	return (
-								<>
-								<section id="contact" className="section-contact-1 bg-900 position-relative pt-150 pb-lg-250 pb-150 overflow-hidden">
+			<>
+					<section id="contact" className="section-contact-1 bg-900 position-relative pt-150 pb-lg-250 pb-250">
 						<div className="text-center">
-  <h3 className="ds-3 mt-3 mb-3 text-primary-1">Get in touch</h3>
+							<h3 className="ds-3 mt-3 mb-3 text-primary-1">Get in touch</h3>
 							<span className="fs-5 fw-medium text-200 mb-4 d-block text-center">
 							I'm always open to new projects and collaborating with innovative minds!
 							</span>
@@ -25,13 +25,14 @@ export default function Contact1() {
 								Contact Me
 							</a>
 							</div>
-				</div>
-				<div className="scroll-move-right position-absolute bottom-0 start-50 translate-middle-x bg-900 overflow-hidden">
-					<div className="wow img-custom-anim-top">
-						<h3 className="stroke fs-280 text-lowercase text-900 mb-0 lh-1">Betty Phipps</h3>
-					</div>
-				</div>
-			</section>
+						</div>
+
+						<div className="scroll-move-right position-absolute bottom-0 start-50 translate-middle-x bg-900" style={{ zIndex: 0 }}>
+							<div className="wow img-custom-anim-top">
+							<h3 className="stroke fs-280 text-lowercase text-900 mb-0 lh-1">Betty Phipps</h3>
+							</div>
+						</div>
+					</section>
 
 		</>
 	)
